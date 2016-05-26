eaabc93381bab2c648fd30dc454ce7ca3c620615
raulnabarret <raulnabarret@gmail.com>
2016-05-26 17:48:21 -0500

    Cargar archivo estático index.html en el servidor

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const http = require('http')
+const fs = require('fs')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
-
 server.on('request', onRequest)
-server.on('listening' onListening)
-
+server.on('listening', onListening)
 server.listen(port)
 
 function onRequest (req, res) {
-	res.end('Hello io.js')
+	let file = fs.readFileSync('public/index.html')
+	res.end(file)
 } 
 
 function onListening () {

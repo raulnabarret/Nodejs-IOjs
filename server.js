b6be7c402b3a25d7ca60caf962c05000476a74b8
raulnabarret <raulnabarret@gmail.com>
2016-05-26 18:39:41 -0500

    Utilizar path y fs asíncrono

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
@@ -10,8 +11,15 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest (req, res) {
-	let file = fs.readFileSync('public/index.html')
-	res.end(file)
+	let fileName = path.join(__dirname, 'public', 'index.html')
+
+	fs.readFile(fileName, function (err, file) {
+		if (err){
+			return res.end(err.message)
+		} 
+
+		res.end(file)
+	})
 } 
 
 function onListening () {

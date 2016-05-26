29ce00c2f10d0e47edde01cf80da2665624bb62a
raulnabarret <raulnabarret@gmail.com>
2016-05-26 17:07:53 -0500

    Agregar callback en el request y en el listen

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const http = require('http')
 
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+const server = http.createServer(function (req, res) {
+	res.end('Hola io.js')
 
-server.listen(port)
+})
+
+server.listen(port, function() {
+	console.log('HTTP 200 on port ' + port)	
+})

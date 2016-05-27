028bdad81508157dee634cf7a1cee0421d494ce6
raulnabarret <raulnabarret@gmail.com>
2016-05-26 19:16:11 -0500

    Servir archivo index.html y app.js a través de funciones

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest (req, res) {
+	let uri = req.url
+
+	if (uri.startsWith('/index') || uri === '/') {
+		return serveIndex(res)
+	}
+
+	if (uri === '/app.js') {
+		return serveApp(res)
+	}
+
+	res.statusCode = 404
+	res.end(`404 not found: ${uri}`)
+} 
+
+function serveIndex (res) {
 	let index = path.join(__dirname, 'public', 'index.html')
 
 	res.setHeader('Content-Type', 'text/html')
@@ -21,8 +36,20 @@ function onRequest (req, res) {
 		res.setHeader('Content-Type', 'text/plain')
 		res.end(err.message)
 	})
+}
 
-} 
+function serveApp (res) {
+	let app = path.join(__dirname, 'public', 'app.js')
+
+	res.setHeader('Content-Type', 'text/javascript')
+	let rs = fs.createReadStream(app)
+	rs.pipe(res)
+
+	rs.on('error', function () {
+		res.setHeader('Content-Type', 'text/plain')
+		res.end(err.message)
+	})
+}
 
 function onListening () {
 	console.log(`HTTP 200 on port ${port}`)	

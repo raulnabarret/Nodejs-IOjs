2b209e2bb08836cc8c2a5215013055e9990e611d
raulnabarret <raulnabarret@gmail.com>
2016-05-30 21:38:34 -0500

    Crear funciones base para conversión de vídeo con async

--- helper/video.js
+++ helper/video.js
@@ -1,13 +1,43 @@
 'use strict'
 
+const async = require('async')
 const EventEmitter = require('events').EventEmitter
 
 module.exports = function (images) {
 	let events = new EventEmitter()
 
-	setTimeout(function () {
-		events.emit('video', 'this will be the encoded video')
-	}, 1000)
+	async.series([
+		decodeImages,
+		createVideo,
+		encodeVideo,
+		cleanUp
+	], convertFinished)
+
+	function decodeImages (done) {
+		done()
+
+	}
+
+	function createVideo (done) {
+		done()
+
+	}
+
+	function encodeVideo (done) {
+		done()
+
+	}
+
+	function cleanUp (done) {
+		done()
+
+	}
+
+	function convertFinished (err) {
+		setTimeout(function () {
+			events.emit('video', 'this will be the encoded video')
+		}, 1000)
+	}
 
 	return events
 }

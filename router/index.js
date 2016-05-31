8b247d46a9f25af2470153f8609f7d889bcf61ab
raulnabarret <raulnabarret@gmail.com>
2016-05-30 21:15:32 -0500

    Crear helper para conversion de vídeo usando EventEmitter

--- router/index.js
+++ router/index.js
@@ -1,7 +1,10 @@
+'use strict'
+
 const path = require('path')
 const st = require('st')
 const course = require('course')
 const jsonBody = require('body/json')
+const helper = require('../helper')
 
 
 const router = course()
@@ -16,10 +19,13 @@ router.post('/process', function (req, res) {
 	jsonBody(req, res, { limit: 3 * 1024 * 1024 }, function (err, body) {
 		if (err) return fail(err, res)
 
-		console.log(body)
+		let converter = helper.convertVideo(body.images)
 
-		res.setHeader('Content-Type', 'application/json')
-		res.end(JSON.stringify({ ok: true}))
+		converter.on('video', function (video){
+			res.setHeader('Content-Type', 'application/json')
+			res.end(JSON.stringify({ video: video }))
+		})
+
 	})
 })
 
